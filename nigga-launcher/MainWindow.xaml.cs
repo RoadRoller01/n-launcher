f2ed62f3991dde626311b388ff97430d22d7fea8
RoadRoller01 <76426234+RoadRoller01@users.noreply.github.com>
2022-05-26 03:54:09 +0300

    Add project files.

---
+++ nigga-launcher/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Net;
+
+namespace nigga_launcher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            var client = new GitHubClient(new ProductHeaderValue("amogusBalls123hentai"));
+            var releases = await client.Repository.Release.GetAll("RoadRoller01", "amogus");
+            var latestAsset = releases[0].Assets[0];
+            // Download with WebClient
+            using var webClient = new WebClient();
+            webClient.Headers.Add(HttpRequestHeader.UserAgent, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36");
+            webClient.Headers.Add(HttpRequestHeader.Accept, "application/octet-stream");
+
+            var latestAssetUri = new Uri(latestAsset.Url);
+            // Download the file
+            webClient.DownloadFileAsync(latestAssetUri, "C:/sus/meow.zip");
+
+            MessageBox.Show(
+                "The latest " + "\n" +
+                latestAsset.Url + "\n" +
+                latestAsset.Name);
+
+        }
+    }
+}
